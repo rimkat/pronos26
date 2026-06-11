073a671ec22489911fcbb852fd87d1c6b49749e1
rimkat <hadirikarim@gmail.com>
2026-06-11 21:24:57 +0200

    Update API URL and Vercel config

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_API_URL;
 export const API = `${BACKEND_URL}/api`;
 
 const api = axios.create({ baseURL: API });
